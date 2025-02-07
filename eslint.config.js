5b2494420f359f7e722a82b378c4c5b9fcd4b379
Hansky Mulya <me@hansky.dev>
2025-02-07 15:25:35 +0700

    chore: add eslint plugin react

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 import js from '@eslint/js';
 import prettier from 'eslint-plugin-prettier';
+import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import globals from 'globals';
@@ -14,12 +15,16 @@ export default tseslint.config(
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    settings: { react: { version: '19.0' } },
     plugins: {
+      react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
       prettier,
     },
     rules: {
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
       ...prettier.configs.recommended.rules,
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
