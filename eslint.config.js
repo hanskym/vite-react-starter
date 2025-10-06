8ab55e8e9976e5184961865d734ced182cc86f0f
Farhan Rinsky Mulya <me@hansky.dev>
2025-10-06 11:32:32 +0700

    chore: update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -3,29 +3,33 @@ import prettier from 'eslint-plugin-prettier';
 import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
+import { defineConfig, globalIgnores } from 'eslint/config';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
-  { ignores: ['dist', '.react-router', 'build'] },
+export default defineConfig([
+  globalIgnores(['dist', '.react-router', 'build']),
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
-    settings: { react: { version: '19.0' } },
+    settings: { react: { version: '19.2' } },
     plugins: {
       react,
       'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
       prettier,
     },
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs['recommended'],
+      reactRefresh.configs.vite,
+    ],
     rules: {
       ...react.configs.recommended.rules,
       ...react.configs['jsx-runtime'].rules,
-      ...reactHooks.configs.recommended.rules,
       ...prettier.configs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
@@ -42,4 +46,4 @@ export default tseslint.config(
       ],
     },
   },
-);
+]);
